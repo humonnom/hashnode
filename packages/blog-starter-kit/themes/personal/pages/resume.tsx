8a3c7a3c6c232c43a7c02e53105987745ceb3fb1
humonnom <juepark42@gmail.com>
2025-12-10 10:20:18 +0900

    Delete unused code

--- packages/blog-starter-kit/themes/personal/pages/resume.tsx
+++ packages/blog-starter-kit/themes/personal/pages/resume.tsx
@@ -1,9 +1,6 @@
-import { useRouter } from 'next/router';
 import ResumePage from '../components/resume/resume-page';
 import data from '../data/resume.json';
 
 export default function Page() {
-	const router = useRouter();
-
 	return <ResumePage type={'instructor'} data={data} />;
 }
